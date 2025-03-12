c89f5bb4529fe3a871552797333b04a7eb34f848
Towfiq <mohammedthawfiq00@gmail.com>
2025-03-13 02:37:04 +0600

    Added sorting sunctionality

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { ProductIdContext } from '../../Provider/ProductIdProvider';
 import DisplayCart from '../DisplayCart/DisplayCart';
@@ -8,6 +8,7 @@ const Cart = () => {
     const {productIdCart} = useContext(ProductIdContext)
     console.log(productIdCart)
     const items = data.filter(item => productIdCart.includes(item.product_id) )
+    const [sortedItems, setSortedItems] = useState(items)
     console.log(items)
 
     let totalPrice = 0
@@ -18,19 +19,24 @@ const Cart = () => {
 
     // console.log(totalPrice)
 
+    const handleSort = () =>{
+        const sortedItemsArr = [...sortedItems].sort((a, b) => b.price - a.price)
+        setSortedItems(sortedItemsArr)
+    }
+
     return (
         <div className='max-w-7xl mx-auto'>
-            <div className='flex justify-between items-center'>
+            <div className='flex justify-between items-center mb-8'>
                 <h5 className='font-bold text-2xl'>Cart</h5>
                 <div className='flex items-center space-x-4'>
                     <h4 className='font-bold text-2xl'>Total cost: $ {totalPrice}</h4>
-                    <button className='py-3 px-6 text-purple border-purple-300 border rounded-4xl'>Sort by price</button>
+                    <button onClick={handleSort} className='py-3 px-6 text-purple border-purple-300 border rounded-4xl'>Sort by price (High to Low)</button>
                     <button className=' py-3 px-6 border-purple-300 border rounded-4xl'>Purchase</button>
                 </div>
             </div>
             <div>
                 {
-                    items.map(item => <DisplayCart key={item.product_id} item={item}></DisplayCart>)
+                    sortedItems.map(item => <DisplayCart key={item.product_id} item={item}></DisplayCart>)
                 }
             </div>
         </div>
